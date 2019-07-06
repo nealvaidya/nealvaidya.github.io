b6bd7f9bc5c7b77921fa5838a2a4874cc0edb918
Neal Vaidya <nv35@duke.edu>
2019-07-06 15:55:50 -0400

    added churchmap

---
+++ churches/precache-manifest.042678fa3f6694671e37adfa0f5dff5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd551fe178fe476a1910ffcc09e912bb",
+    "url": "/churches/index.html"
+  },
+  {
+    "revision": "d07861cdfd9a2698c06b",
+    "url": "/churches/static/css/2.b63e43cf.chunk.css"
+  },
+  {
+    "revision": "6493e5eecf00a9e1b3f6",
+    "url": "/churches/static/css/main.83bc20d8.chunk.css"
+  },
+  {
+    "revision": "d07861cdfd9a2698c06b",
+    "url": "/churches/static/js/2.e3fb53f8.chunk.js"
+  },
+  {
+    "revision": "6493e5eecf00a9e1b3f6",
+    "url": "/churches/static/js/main.d4c06f23.chunk.js"
+  },
+  {
+    "revision": "00ff8e7fad473565804a",
+    "url": "/churches/static/js/runtime~main.0b53fffb.js"
+  },
+  {
+    "revision": "416f28be6c9319094ebf6fa2b41db190",
+    "url": "/churches/static/media/activecross.416f28be.svg"
+  },
+  {
+    "revision": "d7df4b27ecd778e25e9aa19f2f0e3c95",
+    "url": "/churches/static/media/cross.d7df4b27.svg"
+  }
+]);
